2a348589d770caa2c5adcaff0c8cb5bbcb13e22c
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2022-04-04 17:13:09 +0200

    feat: add on module init for migrations

--- movie-service/src/app.module.ts
+++ movie-service/src/app.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { ConfigModule } from '@nestjs/config';
 import { MovieModule } from './movie/movie.module';
 import { GenreModule } from './genre/genre.module';
+import { MikroORM } from '@mikro-orm/core';
 
 @Module({
   imports: [
@@ -12,4 +13,15 @@ import { GenreModule } from './genre/genre.module';
     GenreModule,
   ],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  constructor(private readonly orm: MikroORM) {}
+
+  async onModuleInit() {
+    const migrator = this.orm.getMigrator();
+    const migrations = await migrator.getPendingMigrations();
+
+    if (migrations && migrations.length > 0) {
+      await migrator.up();
+    }
+  }
+}
