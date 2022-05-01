39b9051f0175e8a99312f09e2268eb1771b51483
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2022-05-01 15:32:33 +0200

    test: add authentication module test

---
+++ user-service/src/user/user.controller.spec.ts
@@ -0,0 +1,53 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { MikroORM } from '@mikro-orm/core';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { useDatabaseTestConfig } from '../../test/helpers/database';
+import { UserService } from './user.service';
+import { User } from './entities/user.entity';
+import { UserController } from './user.controller';
+import { createUser } from '../../test/factories/user';
+import { faker } from '@faker-js/faker';
+
+describe('UserController', () => {
+  let userController: UserController;
+  let module: TestingModule;
+  let orm: MikroORM;
+
+  beforeAll(async () => {
+    module = await Test.createTestingModule({
+      imports: [
+        useDatabaseTestConfig(),
+        MikroOrmModule.forFeature({ entities: [User] }),
+      ],
+      controllers: [UserController],
+      providers: [UserService],
+    }).compile();
+
+    userController = module.get<UserController>(UserController);
+    orm = module.get<MikroORM>(MikroORM);
+  });
+
+  beforeEach(async () => {
+    await orm.getSchemaGenerator().refreshDatabase();
+  });
+
+  afterAll(async () => {
+    await module.close();
+    await orm.close();
+  });
+
+  describe('isEmailTaken', () => {
+    it('should return true if the email is taken', async () => {
+      const user = createUser({}, orm);
+      const email = user.email;
+
+      expect(await userController.isEmailTaken(email)).toBe(true);
+    });
+
+    it('should return false if the email is not taken', async () => {
+      expect(await userController.isEmailTaken(faker.internet.email())).toBe(
+        false,
+      );
+    });
+  });
+});
