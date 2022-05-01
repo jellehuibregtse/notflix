484cfd3c48fcdaf1bff7f66221276bfec347467e
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2022-05-01 15:22:46 +0200

    feat: add user controller to check if an email is taken

--- user-service/src/authentication/authentication.module.ts
+++ user-service/src/authentication/authentication.module.ts
@@ -4,7 +4,7 @@ import { PassportModule } from '@nestjs/passport';
 import { UserModule } from '../user/user.module';
 import { Module } from '@nestjs/common';
 import { AuthenticationController } from './authenitcation.controller';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategies/jwt.strategy';
 
