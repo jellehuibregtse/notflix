2ec98be3b13f96d61a43fccc5e296ba3a821616e
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2022-06-02 11:39:31 +0200

    feat: add rate limiter to gateway
    This allows each IP to only put in 100 requests per time window of 10 minutes

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { registerProxyGateway } from './proxy/routes.proxy';
+import rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bodyParser: false });
   app.setGlobalPrefix('api');
+  app.use(
+    rateLimit({
+      windowMs: 10 * 60 * 1000, // 10 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+    }),
+  );
 
   registerProxyGateway(app);
 
